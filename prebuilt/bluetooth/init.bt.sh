eebabf99983caac19adb78fa015705dec2a3dec7
lesleyxxx12 <lesleyvanderlee318@gmail.com>
2016-05-26 20:30:20 +0200

    Enhance Bluetooth logging

--- prebuilt/bluetooth/init.bt.sh
+++ prebuilt/bluetooth/init.bt.sh
@@ -19,26 +19,27 @@ loge ()
 
 failed ()
 {
-  loge "$1: exit code $2"
+  loge "$1: Exit code $2"
   exit $2
 }
 
-#Enable power of csr chip
+#Enable power of CSR chip
+logi "Enable BT Power"
 echo "1" > $BTSTATE
 
 # PS Config with bccmd
 logwrapper /system/bin/bccmd -t bcsp -d $BTUART_PORT -b 115200 psload -r $PSCONFIG
 case $? in
-  0) logi "bccmd init port....done";;
-  *) failed "port: $BTUART_PORT - bccmd failed" $?;
+  0) logi "BCCMD initialization done";;
+  *) failed "BCCMD initialization failed. Port initialization failure: $BTUART_PORT" $?;
      exit $?;;
 esac
 
-# attach HCI 
+# Attach HCI 
 logwrapper /system/bin/hciattach -p $BTUART_PORT bcsp 3000000 flow
 case $? in
-  0) logi "hci attached to : $BTUART_PORT";;
-  *) failed "port: $BTUART_PORT - hciattach failed" $?;
+  0) logi "HCI attach done. Attached to port: $BTUART_PORT";;
+  *) failed "HCI attach failed. Failed to attach to port: $BTUART_PORT" $?;
      exit $?;;
 esac
 
